07c9c536ebba65a8fc59001a41d199d6add8b9bd
Genki Matsunaga <genki.matsunaga@gmail.com>
2020-07-26 11:53:26 +0900

    Add a evaluation function

---
+++ cv/__init__.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+
+
+def r2_cv(model, X_train, y_train, n_folds):
+    kf = KFold(
+        n_folds,
+        shuffle=True,
+        random_state=42
+    ).get_n_splits(X_train)
+    scores = np.sqrt(
+        cross_val_score(
+            model,
+            X_train,
+            y_train,
+            scoring='r2',
+            cv = kf,
+        )
+    )
+    return scores
